98a32091451ac7a11c10b2e4fbc00dfaa99f43c4
Paulo Pinto <paulo.pinto@datadoghq.com>
2023-05-23 13:56:51 +0200

    Fixed sorting, added index

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const movies = JSON.parse(document.querySelector('textarea').value).map(m => {
+let movies = JSON.parse(document.querySelector('textarea').value).map(m => {
     m.title = m.name.replace(/(The|A) (.*)/, '$2, $1') + (m.year ? ` (${m.year})` : '');
     m.header = m.title.match(/^[^A-Z]/) ? '0-9' : m.title[0];
     return m;
-}).sort((a, b) => a.title > b.title);
+});
+movies.sort((a, b) => a.title > b.title ? 1 : -1);
 
 const mk = (tag, text, parent) => {
     const e = document.createElement(tag);
@@ -63,21 +64,27 @@ function pick(ul) {
     show(lis[Math.floor(Math.random() * lis.length)]);
 }
 
+const mainHeader = document.querySelector('h1');
+const index = document.querySelector('#index');
 const content = document.querySelector('#content');
 function refresh(items, title) {
     selected.style.display = 'none';
-    document.querySelector('h1').innerText = `${title} (${items.length})`;
+    mainHeader.innerText = `${title} (${items.length})`;
     content.innerHTML = '';
+    index.innerHTML = '';
     let list = null, lastHeader = null;
+    const addHeader = (m) => {
+        const h2 = mk('h2', m.header, content);
+        h2.id = m.header;
+        const ul = mk('ul', '', content);
+        onclick(h2, () => pick(ul));
+        mk('a', m.header, index).href = `#${m.header}`;
+        return ul;
+    };
     items.forEach(m => {
         if (!list || lastHeader != m.header) {
-            // new header
-            const h2 = mk('h2', m.header, content);
-            h2.id = m.header;
-            const ul = mk('ul', '', content);
+            list = addHeader(m);
             lastHeader = m.header;
-            onclick(h2, () => pick(ul));
-            list = ul;
         }
         const li = mk('li', m.title, list);
         li.movie = m;
@@ -96,4 +103,4 @@ function filter(key, value) {
 }
 
 refresh(movies, 'All movies');
-onclick(document.querySelector('h1'), () => refresh(movies, 'All movies'));
+onclick(mainHeader, () => refresh(movies, 'All movies'));
